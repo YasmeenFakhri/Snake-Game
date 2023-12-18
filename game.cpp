eda08671ccb40fc7715455ea4522c24bd464f218
Haidy Yasser <99363484+Haidyasser@users.noreply.github.com>
2023-12-18 12:17:38 +0200

    add comments

--- game.cpp
+++ game.cpp
@@ -1,18 +1,30 @@
-#include <GL/gl.h>
-#include <GL/glut.h>
-#include <ctime>
-#include "game.h"
+#include <GL/gl.h> // include the header file for OpenGL functions
+#include <GL/glut.h> // include the header file for GLUT library
+#include <ctime> // include the header file for time functions
+#include "game.h" // include the custom header file for game logic
 
-extern int score;
+// define some constants or macros for the magic numbers
+#define MAX_LENGTH 60 // the maximum length of the snake
+#define INIT_LENGTH 5 // the initial length of the snake
+#define INIT_X 20 // the initial x position of the snake
+#define INIT_Y 20 // the initial y position of the snake
+#define EDGE_WIDTH 3.0 // the width of the edge line
+#define RED 1.0,0.0,0.0 // the RGB color of the edge
+#define WHITE 1.0,1.0,1.0 // the RGB color of the grid
+#define BLACK 0.0,0.0,0.0 // the RGB color of the background
+#define OFFSET 2 // the offset from the edge of the grid
+#define UNIT 1 // the unit size of the grid
 
-int gridx, gridy;
-int snake_length = 5;
-int food =1;
-int foodX,foodY;
-short sDirection  = RIGHT ;
-extern int gameOver;
+extern int score; // declare an external variable for the player's score
 
-int posX[60]={20,20,20,20,20} , posY[60]={20,19,18,17,16};
+int gridx, gridy; // declare global variables for the grid size
+int snake_length = INIT_LENGTH; // declare and initialize a global variable for the snake length
+int food =1; // declare and initialize a global variable for the food flag
+int foodX,foodY; // declare global variables for the food position
+short sDirection  = RIGHT ; // declare and initialize a global variable for the snake direction
+extern int gameOver; // declare an external variable for the game over flag
+
+int posX[MAX_LENGTH]={INIT_X,INIT_X,INIT_X,INIT_X,INIT_X} , posY[MAX_LENGTH]={INIT_Y,INIT_Y-1,INIT_Y-2,INIT_Y-3,INIT_Y-4}; // declare and initialize global arrays for the snake positions
 
 void initGrid(int x, int y)//initialize the grid coordinates
 {
@@ -20,94 +32,99 @@ void initGrid(int x, int y)//initialize the grid coordinates
     gridy = y;
 }
 
-void drawGrid()
+void drawGrid() // draw the grid on the window
 {
-    for(int x=0;x<gridx;x++)
+    for(int x=0;x<gridx;x++) // loop through the x coordinates
     {
-        for(int y=0;y<gridy;y++)
+        for(int y=0;y<gridy;y++) // loop through the y coordinates
         {
-            unit(x,y);//draw the hole grid
+            unit(x,y);//draw one unit of the grid
         }
     }
 }
 
-void unit(int x, int y) //one closed square will formed
+void unit(int x, int y) // draw one unit of the grid
 {
-    if(x==0 || y==0 || x==gridx-1 || y==gridy-1)
+    if(x==0 || y==0 || x==gridx-1 || y==gridy-1) // check if the unit is on the edge
     {
-        glLineWidth(3.0); //set the thickness of the edge line
-        glColor3f(1.0,0.0,0.0); // set the color of the edge to be red
+        glLineWidth(EDGE_WIDTH); //set the thickness of the edge line
+        glColor3f(RED); // set the color of the edge to be red
     }
-    else
+    else // otherwise
     {
         glLineWidth(1.0); //set the thickness of the line
-        glColor3f(1.0,1.0,1.0);//set the color of the line to be white
+        glColor3f(WHITE);//set the color of the line to be white
     }
 
     glBegin(GL_POLYGON);  //line_loop mean connected ends
         glVertex2f(x,y); //first point of the square
-        glVertex2f(x+1,y); //second point
-        glVertex2f(x+1,y+1);//third point
-        glVertex2f(x,y+1); //forth point
+        glVertex2f(x+UNIT,y); //second point
+        glVertex2f(x+UNIT,y+UNIT);//third point
+        glVertex2f(x,y+UNIT); //forth point
     glEnd();
 }
-void drawFood()
+void drawFood() // draw the food on the window
 {
 
-    if(food)
-        random(foodX,foodY);
-    food=0;
-    glRectf(foodX,foodY,foodX+1,foodY+1);
+    if(food) // check if the food flag is true
+        random(foodX,foodY); // generate a random position for the food
+    food=0; // set the food flag to false
+    glColor3f(WHITE); // set the color of the food to be white
+    glRectf(foodX,foodY,foodX+UNIT,foodY+UNIT); // draw a rectangle for the food
 
 }
 
-void drawSnake()
+void drawSnake() // draw the snake on the window
 {
-    for(int i=snake_length-1;i>0;i--)
-        {
-        posX[i]=posX[i-1];
-        posY[i]=posY[i-1];
+    for(int i=snake_length-1;i>0;i--) // loop through the snake nodes from the tail to the head
+    {
+        posX[i]=posX[i-1]; // update the x position of the node to the previous node
+        posY[i]=posY[i-1]; // update the y position of the node to the previous node
     }
-    if(sDirection == UP)
-        posY[0]++ ;
-    else if(sDirection == DOWN)
-        posY[0]--;
-    else if(sDirection == RIGHT)
-        posX[0]++;
-    else if(sDirection == LEFT)
-        posX[0]--;
-    for(int i=0;i<snake_length;i++)
-        {
-        if(i == 0)
-            glColor3f(0.0,1.0,0.0);
-        else
-            glColor3f(0.0,0.0,1.0);
-            glRectd(posX[i] , posY[i] , posX[i]+1 , posY[i]+1);
+    switch(sDirection) // switch on the snake direction
+    {
+        case UP: // if the direction is up
+            posY[0]++ ; // increment the y position of the head
+            break; // break the switch
+        case DOWN: // if the direction is down
+            posY[0]--; // decrement the y position of the head
+            break; // break the switch
+        case RIGHT: // if the direction is right
+            posX[0]++; // increment the x position of the head
+            break; // break the switch
+        case LEFT: // if the direction is left
+            posX[0]--; // decrement the x position of the head
+            break; // break the switch
     }
-    if(posX[0]==0 || posX[0] == gridx-1 || posY[0] ==0 || posY[0] == gridy-1)
-        gameOver=1;
-        if (posX[0]==foodX && posY[0]==foodY)
-        {
-           score++;
-           snake_length++;
-           if(snake_length>MAX)
-             snake_length = MAX;
-           food=1;
-        }
+    for(int i=0;i<snake_length;i++) // loop through the snake nodes
+    {
+        if(i == 0) // if the node is the head
+            glColor3f(0.0,1.0,0.0); // set the color to be green
+        else // otherwise
+            glColor3f(0.0,0.0,1.0); // set the color to be blue
+        glRectd(posX[i] , posY[i] , posX[i]+UNIT , posY[i]+UNIT); // draw a rectangle for the node
+    }
+    if(posX[0]==0 || posX[0] == gridx-1 || posY[0] ==0 || posY[0] == gridy-1) // check if the snake hits the edge
+        gameOver=1; // set the game over flag to true
+    if (posX[0]==foodX && posY[0]==foodY) // check if the snake eats the food
+    {
+        score++; // increment the score
+        snake_length++; // increment the snake length
+        if(snake_length>MAX_LENGTH) // check if the snake length exceeds the maximum length
+            snake_length = MAX_LENGTH; // set the snake length to the maximum length
+        food=1; // set the food flag to true
+    }
 
 
 }
 
-void random(int &x,int&y)
+void random(int &x,int&y) // generate a random position for the food
 {
-    int _maxX = gridx-2;
-    int _maxY = gridy-2;
-    int _min=1;
-    srand(time(NULL));
-    x= _min+rand()% (_maxX-_min);
-    y= _min+rand()% (_maxY-_min);
+    int _maxX = gridx-OFFSET; // calculate the maximum x coordinate
+    int _maxY = gridy-OFFSET; // calculate the maximum y coordinate
+    int _min=OFFSET; // calculate the minimum coordinate
+    srand(time(NULL)); // seed the random number generator with the current time
+    x= _min+rand()% (_maxX-_min); // generate a random x coordinate within the range
+    y= _min+rand()% (_maxY-_min); // generate a random y coordinate within the range
 
 }
-
-
-
